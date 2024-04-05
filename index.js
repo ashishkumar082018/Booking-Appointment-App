f340ad8ae63fb4d074892ffe623c3475c35aaa63
ashishkumar082018 <ashishkumar082018@gmail.com>
2024-04-05 18:13:19 +0530

    Task 27 Deleting User Details

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 // Write your code below:
 function handleFormSubmit(event) {
-  window.addEventListner("DOMContentLoaded", () => {
-    axios.get("https://crudcrud.com/api/44e0767bcc64415c84585e4fc5d67c74")
-    .then((result)=>{
-      console.log(result)
-      .catch((error) =>{
-        console.log(error);
-      })
+  window.addEventListener("DOMContentLoaded", () =>{
+     axios.delete("https://crudcrud.com/api/44e0767bcc64415c84585e4fc5d67c74")
+    .then((result) => {
+      console.log(result);
     })
-  })
+    .catch((error) => {
+      console.log(error);
+    })
+  });
 }
 
 // Do not touch the code below
